2b98fd64455e6f0bc092d19755bf45908e47c5d1
Baldez Jr <abaldezjr@gmail.com>
2021-11-16 21:01:04 -0300

    adicionando imagens, divs e cards

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('oi');
+
+function desenha() {
+    let cards = document.getElementsByClassName("card");
+    for (let i in cards) {
+        cards[i].style.left = i % 8 * 165 + "px";
+        cards[i].style.top = i < 8 ? 5 + "px" : 250 + "px";
+    }
+}
+
+function startGame() {
+    desenha();
+}
+
+startGame();
